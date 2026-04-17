e7e92090aedeebbd1fb89e8b6f87434b2978f667
GbarielMota000 <20251AC0080033@estudantes.ifpr.edu.br>
2026-04-17 08:06:10 -0300

    melhorando a segurança

--- src/models/CompanhiaAerea.js
+++ src/models/CompanhiaAerea.js
@@ -5,12 +5,12 @@ class CompaniaAerea {
      * O método construtor é chamado quando um novo passageiro chega ao aeroporto.
      * @param {string} nome - O nome completo da compania.
      * @param {string} cnpj - O documento de identificação único.
-     * @param {string} dataFundação - 
+     * @param {string} dataFundacao - 
      */
-    constructor(nome, cnpj, dataFundação ) {
+    constructor(nome, cnpj, dataFundacao ) {
         this.nome = nome;
         this.cnpj = cnpj;
-        this.dataFundação = dataFundação;
+        this.dataFundacao = dataFundacao;
 
          this.estaEmOperacao = false; 
     }
@@ -20,4 +20,16 @@ class CompaniaAerea {
         console.log(`companhia aerea ${this.nome} está em atividade.`);
     }
 
+        /**
+     * Método para atualizar os dados da companhia caso haja erro de digitação.
+     * @param {string} novoNome - O nome corrigido.
+     */
+    corrigirNome(novoNome) {
+        this.nome = novoNome;
+        console.log(`Nome atualizado para: ${this.nome}`);
+    }
+
 }
+// Exporta a classe para ser usada em outros arquivos (Modularização)
+export default CompaniaAerea;
+
